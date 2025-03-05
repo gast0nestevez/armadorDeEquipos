d2325115ec1086a33502a67c3c035216de493d90
gast0nestevez <gastonestevez08@gmail.com>
2025-03-05 16:37:39 -0300

    Connect input data with algorithm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import TeamsMaker from './algorithm'
 
 function App() {
   const [players, setPlayers] = useState([{ name: '', skill: '' }])
+  const [teams, setTeams] = useState([])
 
   const handleChange = (index, field, value) => {
     const newPlayers = [...players]
@@ -19,13 +21,17 @@ function App() {
     }
   }
 
-  /*const addPlayer = () => {
-    setPlayers([...players, { name: '', skill: '' }])
-  }*/
-
   const submitPlayers = () => {
-    const filtered_players = players.filter(player => player.name.trim() !== '' && player.skill.trim() !== '')
-    console.log('displaying players...', filtered_players)
+    const filtered_players = players
+      .filter(player => player.name.trim() !== '' && player.skill.trim() !== '')
+      .map(player => ({ name: player.name, skill: Number(player.skill) }))
+    //console.log('displaying players...', filtered_players)
+  
+    const teamsMaker = new TeamsMaker()
+    const result = teamsMaker.makeTeams(filtered_players)
+    //console.log(result)
+    setTeams(result)
+
     return
   }
 
@@ -49,15 +55,25 @@ function App() {
             />
           </div>
         ))}
-        {/*<button onClick={addPlayer}>Add Player</button>*/}
+        
         <button onClick={submitPlayers}>
           Submit
         </button>
       </div>
 
-      <div className="equipos">
-          <p>Equipos...</p>
+      <div className="teams">
+        {teams.length > 0 && teams.map((team, index) => (
+          <div key={index} className="team-container">
+            <h3>Equipo {index + 1} ({team.skill})</h3>
+            <ul>
+              {team.players.map((player, i) => (
+                <li key={i}>{player.name}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
+
     </div>
     </>
   )
