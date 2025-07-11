84c5e6e5d9f5b6d106afabb8b8041b1083e0f06c
gast0nestevez <gastonestevez08@gmail.com>
2025-07-10 23:18:52 -0300

    responsive help message

--- src/components/HelpMessage.jsx
+++ src/components/HelpMessage.jsx
@@ -4,7 +4,7 @@ const HelpMessage = () => {
   const [showHelp, setShowHelp] = useState(false)
 
   return (
-    <div className="relative flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <button
         className="w-6 h-6 cursor-pointer"
         onClick={() => setShowHelp(!showHelp)}
@@ -13,7 +13,7 @@ const HelpMessage = () => {
       </button>
 
       {showHelp && (
-        <div className="absolute left-full top-12 -translate-y-1/2 ml-2 w-64 p-2 bg-white border border-blue-300 rounded shadow text-sm z-10">
+        <div className="help-message absolute top-16 ml-2 w-64 p-2 bg-white border border-blue-300 rounded shadow text-sm z-10">
           Ingresá los nombres y puntajes de los jugadores, cuando empieces a escribir el nombre de un jugador se habilitará el cuadro del siguiente. Luego hacé clic en "Armar equipos" para generar. Listo!
         </div>
       )}
