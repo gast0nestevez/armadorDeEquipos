a77194c7731355b1e9dda9015b300c930329a17a
gast0nestevez <gastonestevez08@gmail.com>
2025-09-01 18:12:30 -0300

    fix overflow in teams display

--- src/components/TeamsDisplay.jsx
+++ src/components/TeamsDisplay.jsx
@@ -35,14 +35,14 @@ const Teams = ({ teams, loading }) => {
   return (
     <div className='teams flex flex-col justify-between p-6 bg-blue-100 min-h-full w-full'>
       <h1 className='text-3xl font-bold mb-4 text-center'>Equipos</h1>
-      <div className='flex justify-center gap-6'>
+      <div className='flex justify-center gap-6 overflow-hidden'>
         {teams.map((team, index) => (
-          <div key={index} className='bg-white shadow-md rounded-xl p-6 w-48 text-center'>
+          <div key={index} className='flex flex-col bg-white shadow-md rounded-xl p-6 w-48 text-center'>
             <h3 className='text-xl font-bold mb-2'>Equipo {index + 1}</h3>
             {loading ? (
               <Loader />
             ) : (
-              <ul>
+              <ul className='py-1 max-h-full overflow-y-auto'>
                 {team.players.length > 0 ? (
                   team.players
                     .sort((a, b) => a.name.localeCompare(b.name))
