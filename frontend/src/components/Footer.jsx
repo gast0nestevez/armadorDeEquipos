a8d62cd311f7219a259ad745bcb67f7c62b39778
gast0nestevez <gastonestevez08@gmail.com>
2026-01-08 15:22:40 -0300

    chore: add link to privacy policy

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,6 +14,15 @@ const Footer = ()  => {
           <Github size={16} />
         </div>
       </a>
+      <a 
+        href='https://armador-de-equipos.vercel.app/privacy.html'
+        rel='noopener noreferrer'
+        className='block w-fit px-4 py-3 text-gray-300 hover:text-white transition-colors'
+      >
+        <div className='flex justify-center items-center gap-2 mx-auto max-w-7xl'>
+          <span>Politica de privacidad</span>
+        </div>
+      </a>
     </footer>
   )
 }
