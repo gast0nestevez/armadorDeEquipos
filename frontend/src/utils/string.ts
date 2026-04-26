3ef5c344b478fcbb3a1ca2afaf4d4951d10cea01
gast0nestevez <gastonestevez08@gmail.com>
2026-04-26 14:22:48 -0300

    Add string utils

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -1,3 +1,5 @@
-const capitalize: (s: string) => string = (s: string): string => s[0].toUpperCase() + s.slice(1);
+const capitalize: (s: string) => string = (s: string): string => s.split(' ').map(capitalizeWord).join(' ');
 
-export { capitalize };
+const capitalizeWord: (word: string) => string = (word: string): string => word[0].toUpperCase() + word.slice(1);
+
+export { capitalize, capitalizeWord };
